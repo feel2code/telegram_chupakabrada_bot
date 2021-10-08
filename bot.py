523f3b885cbcd817b2bf1f01d5ee36a9049e182d
minifrukt <89178705098@mail.ru>
2021-10-09 00:04:16 +0300

    added lot of features

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 from conf import *
 
 bot = telebot.TeleBot(name)
@@ -19,6 +20,43 @@ who_is_bot = 'каво'
 abba = 'абоба абобус амогус аабоба'
 note = 'ля ля, ляя ляяя'
 
+k4 = ''
+p4 = ''
+m4 = ''
+
+
+def weather_kzn():
+    global k4
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=kazan&appid='+go_weather)
+    k = r.json()
+    k1 = k['main']
+    k2 = k1['temp']
+    k3 = int(k2 - 273)
+    k4 = str(k3)
+    return k4
+
+
+def weather_spb():
+    global p4
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=498817&appid='+go_weather)
+    p = r.json()
+    p1 = p['main']
+    p2 = p1['temp']
+    p3 = int(p2 - 273)
+    p4 = str(p3)
+    return p4
+
+
+def weather_msk():
+    global m4
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=moscow&appid='+go_weather)
+    m = r.json()
+    m1 = m['main']
+    m2 = m1['temp']
+    m3 = int(m2 - 273)
+    m4 = str(m3)
+    return m4
+
 
 def check(message, diction, answer):
     msg_check = message.text.upper().split()
@@ -35,6 +73,16 @@ def check(message, diction, answer):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    global k4
+    global p4
+    global m4
+    if message.text == '/weather' or message.text == '/weather@chupakabrada_bot':
+        weather_kzn()
+        weather_spb()
+        weather_msk()
+        bot.send_message(message.chat.id, 'Погода в Казани: '+k4+' по Цельсию.')
+        bot.send_message(message.chat.id, 'Погода в Питере: ' + p4 + ' по Цельсию.')
+        bot.send_message(message.chat.id, 'Погода в Москве: ' + m4 + ' по Цельсию.')
     if message.text == '/sticker':
         bot.send_message(message.chat.id, r'Ну вот тебе наклеечки, вот,'
                                           r'пожалуйста, https://t.me/addstickers/Jessieamstaff')
