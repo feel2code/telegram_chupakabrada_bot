38eb9ad1472f0565a41fb20f07abe3e614c16e03
minifrukt <89178705098@mail.ru>
2021-11-20 15:13:17 +0300

    edited script file

--- start.sh
+++ start.sh
@@ -3,4 +3,11 @@ killall screen
 rm -rf /root/tele*
 cd /root/ && git clone https://github.com/feel2code/telegram_chupakabrada_bot
 cp /root/conf.py /root/telegram_chupakabrada_bot/conf.py
+# removing starting script if exists
+if [[ -e start.sh ]]
+then
+rm -rf /root/start.sh
+fi
+# copying starting script loaded from github
+cp /root/telegram_chupakabrada_bot/start.sh /root/start.sh
 screen -dm python3 /root/telegram_chupakabrada_bot/bot.py
