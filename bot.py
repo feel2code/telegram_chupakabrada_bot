4bef3e02f081745f5d50141e498011bb5dca598a
Felix Nabiullin <89178705098@mail.ru>
2022-02-01 21:23:34 +0400

    increased timeout

--- bot.py
+++ bot.py
@@ -194,4 +194,4 @@ def chat_id(message):
         bot.send_message(message.chat.id, chat_id_var)
 '''
 
-bot.polling(none_stop=True, interval=0, timeout=400)
+bot.polling(none_stop=True, interval=0, timeout=500)
