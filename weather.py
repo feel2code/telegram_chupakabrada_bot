df619614a5f91c0b5478a9d208bf2ef5e06e1273
Felix Nabiullin <89178705098@mail.ru>
2022-02-08 13:19:32 +0400

    general updates

--- weather.py
+++ weather.py
@@ -1,11 +1,11 @@
 import telebot
 import requests
 from conf import *
-from sys import argv
+# from sys import argv
 
-script, chat = argv
+chat = "442295142"
+# script, chat = argv
 bot = telebot.TeleBot(name)
-weather_url = 'https://api.openweathermap.org/data/2.5/weather?'
 
 
 def weather(id: str) -> str:
@@ -16,8 +16,8 @@ def weather(id: str) -> str:
         )
     ).json()
     temp_farenheit = (requestings['main'])['temp']
-    returned_temp = str(int(temp_farenheit - 273))
-    return returned_temp
+    temp_celsius = str(int(temp_farenheit - 273))
+    return temp_celsius
 
 
 k4 = weather('q=kazan')
@@ -33,38 +33,35 @@ full_weather_list = [int(item) for item in full_weather_list]
 full_weather_dict = {k4: 'Казань', p4: 'Питер',
                      m4: 'Москва', e4: 'Екб',
                      b4: 'Батуми', t4: 'Тбилиси'}
-inverted_weather_dict = {'Казань': k4, 'Питер': p4,
-                         'Москва': m4, 'Екб': e4,
-                         'Батуми': b4, 'Тбилиси': t4}
 
-# max temp
+# max/min temp
 max_weather = max(full_weather_list)
-# min temp
 min_weather = min(full_weather_list)
-
 
 what_to_send = (
     'Ну шо, с добрим утречком всех, мои зяблики, маи родненькие!\n\n'
     'Вот вам ваша пагода па расписанию, палучаица:\n')
 
 
-def weather_send(temp, city) -> None:
+def weather_send(temp: str):
     '''Checking max or min temp and send emoji near temp'''
     global what_to_send
+    temp = str(temp)
+    temp_send = str(temp)
     if int(temp) >= 0 and int(temp) < 10:
-        temp = temp.replace(temp, ' ' + temp)
-    what_to_send += ('\n' + temp + ' °C · ' + city)
-    if full_weather_dict[str(min_weather)] == city:
+        temp_send = temp.replace(temp, '  ' + temp)
+    elif int(temp) < 0 and int(temp) > - 10:
+        temp_send = temp.replace(temp, ' ' + temp)
+    elif int(temp) > 10:
+        temp_send = temp.replace(temp, ' ' + temp)
+    what_to_send += ('\n' + temp_send + ' °C · ' + full_weather_dict[temp])
+    if full_weather_dict[str(min_weather)] == full_weather_dict[temp]:
         what_to_send += ' ❄️'
-    elif full_weather_dict[str(max_weather)] == city:
+    elif full_weather_dict[str(max_weather)] == full_weather_dict[temp]:
         what_to_send += ' 🔥'
 
 
-weather_send(k4, full_weather_dict[k4])
-weather_send(p4, full_weather_dict[p4])
-weather_send(m4, full_weather_dict[m4])
-weather_send(e4, full_weather_dict[e4])
-weather_send(b4, full_weather_dict[b4])
-weather_send(t4, full_weather_dict[t4])
+for city_weather in full_weather_list:
+    weather_send(city_weather)
 
 bot.send_message(chat_id=chat, text=what_to_send)
