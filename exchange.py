4f0334e7b5dd70f05ba1c4be63f0fa8a40757217
Felix Nabiullin <89178705098@mail.ru>
2022-02-24 23:09:57 +0400

    some changes on exchange course info

--- exchange.py
+++ exchange.py
@@ -16,22 +16,37 @@ def get_usd_course():
     data = soup.find(
         'div', class_='chart__info__row js-ticker'
         ).find('span', class_='chart__info__sum').text
-    data = data.replace('₽', '').replace(' ', '').replace(',', '.')
-    rate = int(float(data))
-    return rate
+    float_rate = float(
+        data.replace('₽', '').replace(' ', '').replace(',', '.')
+    )
+    int_rate = int(float_rate)
+    rate_remain = float_rate - int_rate
+    if int(rate_remain*10) in range(0, 4) or rate_remain in range(8, 10):
+        return int_rate
+    else:
+        return None
 
 
 def check_course():
     rate = get_usd_course()
-    cursor.execute("select course_value from course where course_name='usd'; ")
-    last_rate = cursor.fetchone()[0]
-    if rate != last_rate:
+    if rate is not None:
         cursor.execute(
-            f"update course set course_value={rate} where course_name='usd'; "
+            "select course_value from course where course_name='usd'; "
         )
-        connection_to_db.commit()
-        bot = telebot.TeleBot(bot_token)
-        bot.send_message(chat_id=home_telega, text=f'Далар уже па {rate}₽')
+        last_rate = cursor.fetchone()[0]
+        if rate != last_rate:
+            cursor.execute(
+                f"update course set course_value={rate} "
+                f"where course_name='usd'; "
+            )
+            connection_to_db.commit()
+            bot = telebot.TeleBot(bot_token)
+            bot.send_message(
+                chat_id=home_telega,
+                text=f'Далар уже па {rate} ₽'
+            )
+        else:
+            pass
     else:
         pass
 
