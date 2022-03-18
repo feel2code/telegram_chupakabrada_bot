243c6e021223b92500865050972cefd30a1052c2
Felix <90648171+feel2code@users.noreply.github.com>
2022-03-18 23:39:22 +0400

    update parameters

--- markov/aboba.py
+++ markov/aboba.py
@@ -6,6 +6,6 @@ def markov(message):
     markov_text.write(f'{message.text}. ')
     markov_text.close()
     text = open('markov/markov.txt', encoding='utf8').read()
-    text_model = markovify.Text(text)
+    text_model = markovify.Text(text, state_size=3)
     for i in range(1):
         return text_model.make_sentence(tries=50)
