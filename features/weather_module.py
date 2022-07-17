94651a7bb8b14709ad8064ef5953dde500af6127
Felix Nabiullin <89178705098@mail.ru>
2022-07-17 13:55:59 +0400

    refactoring and adding emojis instead of text

--- features/weather_module.py
+++ features/weather_module.py
@@ -9,16 +9,17 @@ from selects import simple_query
 
 
 def weather_in_city(message):
-    city = message.text.replace('/weather ', '').replace(' ', '-')
+    """
+    Constantly get temperature for any city without saving to DB and send weather to chat
+    :param message: message from telegram
+    :return: None
+    """
+    city_name = message.text.replace('/weather ', '').replace(' ', '-')
     try:
-        req = requests.get(
-            'https://api.openweathermap.org/data/2.5/weather?q=' + city
-            + '&appid=' + weather_token).json()
-        city_temp = str(int((req['main'])['temp'] - 273))
-        what_to_send = f'{simple_query(112)}\n {city_temp} °C {city}'
+        city_temp = weather(city_name)
+        bot.send_message(message.chat.id, f'{simple_query(112)}\n {city_temp} °C {city_name}')
     except KeyError:
-        what_to_send = simple_query(111)
-    bot.send_message(message.chat.id, what_to_send)
+        bot.send_message(message.chat.id, simple_query(111))
 
 
 def weather(city_name: str) -> str:
@@ -47,10 +48,25 @@ def forecast(city_name: str) -> tuple:
     ).json()
     temp_celsius = str(int((response['list'][0]['main']['feels_like']) - 273))
     condition = response['list'][0]['weather'][0]['description'].lower()
+    condition_emoji = {
+        'ясно': '☀️',
+        'небольшая облачность': '🌤',
+        'облачно с прояснениями': '🌥',
+        'пасмурно': '☁️',
+        'небольшой дождь': '🌦',
+        'переменная облачность': '⛅'
+    }
+    # ☀️🌤⛅️⛈🌧🌦☁️🌥🌩🌨❄️
+    condition = condition_emoji[condition] if condition in condition_emoji else condition
     return temp_celsius, condition
 
 
 def add_city(message):
+    """
+    Adds city to DB if this city really exists and send status to chat
+    :param message: message from telegram
+    :return: None
+    """
     chat_id = str(message.chat.id)
     city_name = str(
         message.text).replace('/add ', '').replace(' ', '-').upper()
@@ -63,13 +79,17 @@ def add_city(message):
         cur.execute("insert into cities (chat_id, city_name) "
                     "values (%s, %s)", (chat_id, city_name))
         conn_db.commit()
-        what_to_send = city_name + ' ' + simple_query(121)
+        bot.send_message(message.chat.id, f'{city_name} {simple_query(121)}')
     else:
-        what_to_send = simple_query(119)
-    bot.send_message(message.chat.id, what_to_send)
+        bot.send_message(message.chat.id, simple_query(119))
 
 
 def delete_city(message):
+    """
+    Deletes city from DB and send status to chat
+    :param message: message from telegram
+    :return: None
+    """
     chat_id = str(message.chat.id)
     city_name = message.text.replace('/delete ', '').replace(' ', '-').upper()
     try:
@@ -96,20 +116,32 @@ def delete_city(message):
 
 
 def add_temp_to_db(city_name, chat):
+    """
+    Gets current and forecast temperatures, conditions and inserts it to DB
+    :param city_name: city name or id
+    :param chat: chat from telegram
+    :return: None
+    """
     temp = weather(city_name)
     expected, condition = forecast(city_name)
-    cur.execute(
-        f"""update cities
-            set temp={temp}, expected_day_temp={expected},
-                condition='{condition}'
-            where city_name='{city_name}' and chat_id='{chat}';"""
-    )
+    cur.execute(f"""update cities
+                    set temp={temp}, expected_day_temp={expected},
+                        condition='{condition}'
+                    where city_name='{city_name}' and chat_id='{chat}';""")
     conn_db.commit()
 
 
 def weather_send(chat_id, city_db, min_weather, max_weather, length, is_forecast):
-    global what_to_send
-    """Checking max or min temp and send emoji near temp."""
+    """
+    Checking max or min temp and places emoji near temp
+    :param chat_id: chat id
+    :param city_db: city name
+    :param min_weather: min temperature from DB
+    :param max_weather: max temperature from DB
+    :param length: if length > 1 adds emoji
+    :param is_forecast: selects from DB forecast or current weather
+    :return:
+    """
     cur.execute(
         f"""SELECT temp, expected_day_temp, condition
             FROM cities
@@ -124,23 +156,22 @@ def weather_send(chat_id, city_db, min_weather, max_weather, length, is_forecast
         temp_spaces = ' '
     else:
         temp_spaces = ''
-    what_to_send += f"\n ` {temp_spaces}{temp}° · {city_db} ({condition})`"
+    what_to_send = f"\n ` {temp_spaces}{temp}° {condition} {city_db}`"
     if length > 1:
         if temp == min_weather:
-            what_to_send += ' ❄️'
+            what_to_send += '- 🥶️️'
         elif temp == max_weather:
-            what_to_send += ' 🔥'
+            what_to_send += '- 🥵'
+    return what_to_send
 
 
 def get_weather_list(chat_id):
     """getting cities list from DB."""
-    global what_to_send
-    what_to_send = ''
     if datetime.now().hour in range(0, 7):
-        what_to_send = simple_query(128) + '\n'
+        weather_message = simple_query(128) + '\n'
         is_forecast = True
     else:
-        what_to_send += simple_query(122) + '\n'
+        weather_message = simple_query(122) + '\n'
         is_forecast = False
     cur.execute(
         f"SELECT city_name FROM cities "
@@ -152,24 +183,26 @@ def get_weather_list(chat_id):
         city_db = str((fetched_from_db[i])[0])
         add_temp_to_db(city_db, chat_id)
     # find max and min weather in cities list
+    max_min_temp = 'expected_day_temp' if is_forecast else 'temp'
     if len(fetched_from_db) != 0:
         cur.execute(
-            f"SELECT max(temp), min(temp) FROM cities "
-            f"where chat_id='{chat_id}';"
+            f"""SELECT max({max_min_temp}), min({max_min_temp})
+                FROM cities
+                where chat_id='{chat_id}';"""
         )
         max_min_weather = cur.fetchall()[0]
         # getting each city and temp from db
         for i in range(len(fetched_from_db)):
             city_db = str((fetched_from_db[i])[0])
-            weather_send(chat_id,
-                         city_db,
-                         int(max_min_weather[1]),
-                         int(max_min_weather[0]),
-                         len(fetched_from_db),
-                         is_forecast)
+            weather_message += weather_send(chat_id,
+                                            city_db,
+                                            int(max_min_weather[1]),
+                                            int(max_min_weather[0]),
+                                            len(fetched_from_db),
+                                            is_forecast)
     else:
-        what_to_send = simple_query(116)
+        weather_message = simple_query(116)
     bot.send_message(
         chat_id=chat_id,
-        text=what_to_send,
+        text=weather_message,
         parse_mode='Markdown')
