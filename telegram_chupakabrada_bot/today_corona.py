14e6e4097cbe00df29adb44c0453d56f436efc42
feel2code <89178705098@mail.ru>
2022-11-16 16:16:59 +0400

    refactoring coronavirus info module

--- telegram_chupakabrada_bot/today_corona.py
+++ telegram_chupakabrada_bot/today_corona.py
@@ -1,13 +1,20 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
 from connections import bot
 
 
-def coronavirus(chat: int):
+def coronavirus(chat: str):
     soup = BeautifulSoup(requests.get('https://стопкоронавирус.рф/information/').text, "html.parser")
     found = str(str(soup.select('cv-stats-virus')).split(':stats-data=')[1]).split(',')
     sick_change = str(found[1]).replace('"sickChange":"', '').replace('"', '')
     died_change = str(found[5]).replace('"diedChange":"', '').replace('"', '').replace(r'\u00a0', ' ')
     what_to_send = f'Корона тайм, зяблс. За сегодня в России:\nЗаболевших {sick_change} \nСмертей {died_change} \n'
     bot.send_message(chat_id=chat, text=what_to_send)
+
+
+if __name__ == '__main__':
+    coronavirus(os.getenv('HOME_TELEGA'))
+
