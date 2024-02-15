c0b3a3cff6c8cbcc585eda9fc75c896cea5e101e
feel2code <feel2code@proton.me>
2024-02-15 16:56:24 +0400

    deploy setup

---
+++ telegram_chupakabrada_bot/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+crontab misc/crontab.txt
+
+mkdir misc/voices && mv /misc/voices.tar.xz misc/voices/voices.tar.xz
+cd misc/voices
+tar -xvf voices.tar.xz
+cd ..
+
+cp tel_bot.service /etc/systemd/user/tel_bot.service
+systemctl enable tel_bot.service
+systemctl start tel_bot.service
+systemctl daemon-reload
+
