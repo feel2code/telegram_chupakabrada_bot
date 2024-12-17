291000ea4c77f6b04be173f59e14317f4e97d9f6
feel2code <feel2code@proton.me>
2024-12-18 03:39:51 +0400

    relative holidays fix

--- telegram_chupakabrada_bot/holiday.py
+++ telegram_chupakabrada_bot/holiday.py
@@ -1,10 +1,28 @@
 from calendar import MONDAY, Calendar
-from datetime import datetime, timezone
+from datetime import date, datetime
 
 import requests
 from bs4 import BeautifulSoup
 
 from connections import SQLUtils, bot
+
+
+def is_date_is_last_weekday_of_month(checking_date: date, weekday: int) -> int:
+    """Check if the date is the last weekday of the month."""
+    month_calendar = Calendar(firstweekday=MONDAY).monthdayscalendar(
+        checking_date.year, checking_date.month
+    )
+    months_weekdays = [week[weekday] for week in month_calendar if week[weekday] != 0]
+    return int(checking_date.day == months_weekdays[-1])
+
+
+def current_weekday_counter_in_month(check_date: date):
+    """Returns current weekday counter in month."""
+    return sum(
+        1
+        for i in range(1, check_date.day + 1)
+        if date(check_date.year, check_date.month, i).weekday() == check_date.weekday()
+    )
 
 
 def get_holidays_from_db(message):
@@ -20,19 +38,15 @@ def get_holidays_from_db(message):
     cur_day = datetime.today().day
     cur_month = datetime.today().month
     cur_weekday = datetime.today().weekday()
-    month_calendar = Calendar(firstweekday=MONDAY).monthdayscalendar(
-        datetime.now(timezone.utc).year, cur_month
-    )
-    months_weekdays = [
-        week[cur_weekday] for week in month_calendar if week[cur_weekday] != 0
-    ]
-    day_idx = months_weekdays.index(cur_day) + 1
-    is_last = 1 if months_weekdays[-1] == cur_day else 0
+    weekday_counter = current_weekday_counter_in_month(datetime.today())
+    is_last = is_date_is_last_weekday_of_month(datetime.today(), cur_weekday)
+
     additional_condition = (
-        f"and week_num={day_idx}" if not is_last else f"and is_last={is_last}"
+        f"and week_num={weekday_counter}" if not is_last else f"and is_last={is_last}"
     )
     fetched = db_conn.query(
-        f"""with base_holidays as (select holiday_name from (
+        f"""with base_holidays as (
+            select holiday_name from (
                         select
                             cast(strftime('%d', dt) as integer) as day,
                             cast(strftime('%m', dt) as integer) as month,
@@ -45,15 +59,16 @@ def get_holidays_from_db(message):
                             '🌍 ' || holiday_name as holiday_name
                         from holidays_iso iso
                         ) as holidays
-                    where day={cur_day} and month={cur_month}),
-                relative_holidays as (
-                        select 
-                            '🇷🇺 ' || holiday_name as holiday_name
-                        from holidays_ru_relative hrr
-                        where day_num={cur_weekday + 1}
-                              and cast(strftime('%m', dt) as integer)={cur_month}
-                        {additional_condition}
-                    )
+                    where day={cur_day} and month={cur_month}
+            ),
+            relative_holidays as (
+                select 
+                    '🇷🇺 ' || holiday_name as holiday_name
+                from holidays_ru_relative hrr
+                where day_num={cur_weekday + 1}
+                and cast(strftime('%m', dt) as integer)={cur_month}
+                {additional_condition}
+            )
                 select * from base_holidays
                 union all
                 select * from relative_holidays;"""
